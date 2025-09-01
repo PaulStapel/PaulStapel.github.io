b54c0525b5587650481a3bc4d6031a2b1c55fcb3
Paul <113509667+PaulStapel@users.noreply.github.com>
2025-09-01 23:23:51 +0800

    mobie

--- static/js/app.js
+++ static/js/app.js
@@ -278,7 +278,7 @@ document.addEventListener("DOMContentLoaded", () => {
       if (col === 0) {
         xShift = Math.random() * 20; // left column shift inward
         if(isMobile()){
-          xShift = 5;
+          xShift = 10;
         }
       } else if (col === columns - 1) {
         xShift = -(Math.random() * 20); // right column shift inward
